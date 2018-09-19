d6ba09e8e97568354040f95e1b000d2b737fc8ef
Vinícius Fragoso <vfragosop@gmail.com>
2018-09-19 15:08:12 -0300

    Improve header responsiveness

--- panel/src/components/Home.vue
+++ panel/src/components/Home.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="uk-flex uk-flex-column uk-padding">
-    <div class="uk-text-right">
-      Olá Xandão,
-      <router-link class="uk-link-reset uk-text-bold" :to="{ name: 'logout' }">
-        Sair <span data-uk-icon="icon: sign-out"/>
-      </router-link>
-    </div>
-    <div class="uk-text-center">
-      <img class="uk-width-1-3" src="/logo.png"/>
-    </div>
     <div class="uk-flex uk-flex-none" data-uk-grid>
-      <!-- TODO: Period is disabled because we added expenses API in the last minute -->
-      <div class="uk-flex-first@s uk-width-1-1 uk-width-1-2@s">
+      <div class="uk-flex-last@s uk-text-right uk-width-1-1 uk-width-1-2@s">
+        Olá Xandão,
+        <router-link class="uk-link-reset uk-text-bold" :to="{ name: 'logout' }">
+          Sair <span data-uk-icon="icon: sign-out"/>
+        </router-link>
+      </div>
+      <div class="uk-text-center uk-flex-first@s uk-width-1-1">
+        <img class="uk-width-1-1 uk-width-1-2@s uk-width-1-3@m" src="/logo.png"/>
+      </div>
+      <div class="uk-width-1-1 uk-width-1-2@s">
         <ul class="uk-subnav uk-subnav-pill">
           <li :class="{ 'uk-active': period === 1 }">
             <a @click.prevent="period = 1">Hoje</a>
